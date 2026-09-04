d4b92e2ae3bc5924f6e5b6232011f745c4092f71
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-09-04 22:40:00 +0200

    plugins standardization

--- lib/src/Mediator.ts
+++ lib/src/Mediator.ts
@@ -66,11 +66,19 @@ export default class MediatorCommands extends Mediator<iEventsMinimal & {
         return Promise.resolve();
     }
 
+    // private
+
+    private _readPublic (relativePath: string): Promise<string> {
+
+        return readFile(join(__dirname, "..", "..", "public", relativePath), "utf-8");
+
+    }
+
     // front files
 
     public getFrontIndex (): Promise<operations["getFrontIndex"]["responses"]["200"]["content"]["text/html"]> {
 
-        return readFile(join(__dirname, "..", "..", "public", "index.html"), "utf-8").then((content: string): string => {
+        return this._readPublic("index.html").then((content: string): string => {
 
             return content
 
@@ -84,7 +92,7 @@ export default class MediatorCommands extends Mediator<iEventsMinimal & {
 
     public getFrontApp (): Promise<operations["getFrontApp"]["responses"]["200"]["content"]["application/javascript"]> {
 
-        return readFile(join(__dirname, "..", "..", "public", "dist", "bundle.min.js"), "utf-8").then((content: string): string => {
+        return this._readPublic(join("dist", "bundle.min.js")).then((content: string): string => {
 
             return content
 
@@ -97,7 +105,7 @@ export default class MediatorCommands extends Mediator<iEventsMinimal & {
     }
 
     public getFrontAppMap (): Promise<string> { // tricks return to avoid costful parsing
-        return readFile(join(__dirname, "..", "..", "public", "dist", "bundle.min.js.map"), "utf-8");
+        return this._readPublic(join("dist", "bundle.min.js.map"));
     }
 
     // <api>
